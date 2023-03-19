5185daacdc25c16b2df30ea8ce93fa3285bf0e47
angelomutti <amutti.dev@gmail.com>
2023-03-18 23:24:35 -0300

    Initial commit

---
+++ src/test/java/com/mutti/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutti.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
